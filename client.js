76cb7972eb94fcc0a3178f3cd4734ce14ba60120
aderibigbeganiu <aderibigbeganiu@gmail.com>
2022-12-26 01:52:06 +0100

    initial commit

---
+++ client.js
@@ -0,0 +1,32 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync("todo.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const client = new todoPackage.Todo(
+  "localhost:40000",
+  grpc.credentials.createInsecure()
+);
+
+const text = process.argv[2];
+client.createTodo(
+  {
+    id: -1,
+    text: text,
+  },
+  (err, response) => {
+    console.log("Receive from server " + JSON.stringify(response));
+  }
+);
+
+client.readTodos({}, (err, response) => {
+  console.log("Receive from server " + JSON.stringify(response));
+});
+
+const call = client.readTodosStream();
+call.on("data", (item) => {
+  console.log("Receive from server " + JSON.stringify(item));
+});
+
+call.on("end", (e) => console.log("server done!"));
